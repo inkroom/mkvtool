1dda2dc9f0db8f9bf3cf4db31d42b8b95f5fba08
inkroom <enpassPixiv@protonmail.com>
2026-07-28 21:31:19 +0800

    update ci

--- src-tauri/ffmpeg/build-windows.sh
+++ src-tauri/ffmpeg/build-windows.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 set -eu
-
+FFMPEG_ARCH="win64"
 script_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 src_tauri_dir=$(CDPATH= cd -- "$script_dir/.." && pwd)
-output_dir=$(mktemp -d)
+output_dir="artifacts/bin"
 
-cleanup() {
-  rm -rf "$output_dir"
-}
+FFMPEG_REF="7.1"
+# 修改构建参数
+FF_CONFIGURE="--enable-static --disable-shared  --disable-everything --disable-programs --enable-ffmpeg --enable-ffprobe --disable-avdevice --enable-avfilter --disable-swresample --disable-swscale  --disable-network --disable-doc --disable-debug --enable-protocol=file,pipe --enable-demuxer=matroska,ass,srt --enable-muxer=matroska,ass,srt --enable-decoder=ass,ssa,subrip --enable-encoder=ass,ssa,subrip"
+cat <<EOF > Dockerfile.tmp
+    FROM ghcr.io/ffmpeg-builds/$FFMPEG_ARCH-lgpl-$FFMPEG_REF
+    ENV FF_CONFIGURE="$FF_CONFIGURE"
+EOF
 
-trap cleanup EXIT
+docker build . -t ghcr.io/ffmpeg-builds/$FFMPEG_ARCH-lgpl-$FFMPEG_REF -f Dockerfile.tmp
 
-docker buildx build \
-  --output "type=local,dest=$output_dir" \
-  --file "$script_dir/Dockerfile.windows" \
-  "$script_dir"
-mkdir -p "$src_tauri_dir/binaries"
+
+git clone https://github.com/BtbN/FFmpeg-Builds
+cd FFmpeg-Builds
+
+./build.sh $FFMPEG_ARCH lgpl $FFMPEG_REF
+
 
 install -m 755 "$output_dir/ffmpeg.exe" "$src_tauri_dir/binaries/ffm-x86_64-pc-windows-msvc.exe"
 install -m 755 "$output_dir/ffprobe.exe" "$src_tauri_dir/binaries/ffp-x86_64-pc-windows-msvc.exe"
