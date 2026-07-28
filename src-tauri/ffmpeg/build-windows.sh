7941dc8e44ef48d90958a3d4b61c0900a5edbeb3
inkroom <enpassPixiv@protonmail.com>
2026-07-28 21:32:48 +0800

    update ci

--- src-tauri/ffmpeg/build-windows.sh
+++ src-tauri/ffmpeg/build-windows.sh
@@ -9,7 +9,7 @@ FFMPEG_REF="7.1"
 # 修改构建参数
 FF_CONFIGURE="--enable-static --disable-shared  --disable-everything --disable-programs --enable-ffmpeg --enable-ffprobe --disable-avdevice --enable-avfilter --disable-swresample --disable-swscale  --disable-network --disable-doc --disable-debug --enable-protocol=file,pipe --enable-demuxer=matroska,ass,srt --enable-muxer=matroska,ass,srt --enable-decoder=ass,ssa,subrip --enable-encoder=ass,ssa,subrip"
 cat <<EOF > Dockerfile.tmp
-    FROM ghcr.io/ffmpeg-builds/$FFMPEG_ARCH-lgpl-$FFMPEG_REF
+    FROM ghcr.io/btbn/ffmpeg-builds/$FFMPEG_ARCH-lgpl-$FFMPEG_REF
     ENV FF_CONFIGURE="$FF_CONFIGURE"
 EOF
 
